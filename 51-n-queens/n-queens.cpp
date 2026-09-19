635231c627d51a6edcf0ce4f64b26f9755e8afa1
Sonal Thakur <st2663899@gmail.com>
2026-09-19 20:44:05 +0530

    Time: 3 ms (61.23%) | Memory: 10 MB (99.68%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     bool isSafe(vector<string> &board,int row,int col,int n){
+        //for horizontal ke liye
      for(int j=0;j<n;j++){
         if(board[row][j]=='Q'){
             return false;
         }
      }
+     //for vertical ke liye
      for(int i=0;i<n;i++){
         if(board[i][col]=='Q'){
             return false;
         }
      }
+     //left diagonal ke liye
      for(int i=row,j=col;i>=0 && j>=0;i--,j--){
         if (board[i][j]=='Q'){
             return false;
         }
      }
+     //right diagonal ke liye
      for (int i=row,j=col;i>=0 && j>=0 && j<n ;i--,j++){
         if (board[i][j]=='Q'){
             return false;
